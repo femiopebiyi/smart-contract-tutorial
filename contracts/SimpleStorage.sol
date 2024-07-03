24ba7dddfb3d2a708ad90e888bddf71a3e66eb8b
femiopebiyi <opebiyibiodun10@gmail.com>
2024-07-03 18:00:13 +0100

    first commit, types in solidity

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract SimpleStorage {
+     //boolean, uint, int, address, bytes
+     uint256 favNum = 123;
+     string favNumber = 'opebiyi';
+     int256 favInt = 5;
+     address myAddress = 0x9c219c68B32f45027eBA048C26A21742d970c626;
+     bytes32 favoriteBytes = "cat";
+
+}
