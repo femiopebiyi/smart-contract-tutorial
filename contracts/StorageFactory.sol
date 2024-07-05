dea60a3e1c59464d4b82d85b0c635fffc89a5cc8
femiopebiyi <opebiyibiodun10@gmail.com>
2024-07-05 16:25:19 +0100

    importing contracts into other contracts

---
+++ contracts/StorageFactory.sol
@@ -0,0 +1,15 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "contracts/SimpleStorage.sol";
+
+contract StorageFactory{
+
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+}
