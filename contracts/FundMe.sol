a1288e9f52a84d1712714c176f174578b364fc4f
femiopebiyi <opebiyibiodun10@gmail.com>
2024-07-07 17:32:46 +0100

    floating point maths in solidity

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -8,22 +8,33 @@ pragma solidity ^0.8.8;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe{
-   uint256 public minimumUsd = 50;
+   uint256 public minimumUsd = 50 * 1e18;
 
      function fund() public payable {
         //set minimum find amount
-        require(msg.value >= 1e18, "didnt send enough");
+        require(getConversionRate(msg.value) >= minimumUsd, "didnt send enough");
      }
 
-     function getPrice() public {
+     function getPrice() public view returns (uint256){
       // ABI 
       // Adrress 0x694AA1769357215DE4FAC081bf1f309aDC325306
+
+      AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+      (,int256 answer,,,) = priceFeed.latestRoundData();
+
+      return uint256(answer * 1e10);
      }
 
      function getVersion() public view returns (uint256) {
       AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
       return priceFeed.version();
      }
+
+     function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+      uint256 ethPrice = getPrice();
+      uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+      return ethAmountInUsd;
+   }
 
     //  function withdraw() public {}
 }
